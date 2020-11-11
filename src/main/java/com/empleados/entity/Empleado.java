71e5f8ce843f53c508cdbbe30f44cacc97becf56
fenix8908 <marialuci1019@gmail.com>
2020-11-10 20:47:34 -0500

    Creacion de paquetes y de la entidad Empleado

---
+++ src/main/java/com/empleados/entity/Empleado.java
@@ -0,0 +1,12 @@
+package com.empleados.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "empleado")
+public class Empleado {
+	private int id;
+	
+	
+}
